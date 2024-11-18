7a92419dce80eb4380c7584cc6dfc2f53d771aec
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2024-11-18 22:19:58 +0200

    Complete DeleteDepartment action

---
+++ SchoolProject.Core/Mapping/DepartmentMapping/CommandMapping/DeleteDepartmentMapping.cs
@@ -0,0 +1,14 @@
+﻿using SchoolProject.Core.Features.Departments.Commands.Models;
+using SchoolProject.Domain.Entities;
+
+namespace SchoolProject.Core.Mapping.DepartmentMapping
+{
+    public partial class DepartmentProfile
+    {
+        public void DeleteDepartmentMapping()
+        {
+            CreateMap<DeleteDepartmentCommand, Department>()
+                .ForMember(dest => dest.DID, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
